b521d43ad35062db59b1623764fa2b184d8f1428
tzisrael@deloitte.co.il <tzisrael@deloitte.co.il>
2023-06-25 10:36:36 +0000

    mount and create view

---
+++ vw_real_estate.sql
@@ -0,0 +1,11 @@
+-- Databricks notebook source
+-- MAGIC %sql
+-- MAGIC
+-- MAGIC -- Create a temporary view
+-- MAGIC CREATE OR REPLACE TEMPORARY VIEW vw_real_estate
+-- MAGIC USING csv
+-- MAGIC OPTIONS (
+-- MAGIC   path "/mnt/real_estate_short_sample_5k/real_estate_short_sample_5k.csv",
+-- MAGIC   header "true"
+-- MAGIC );
+-- MAGIC

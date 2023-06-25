e7f8f9fb6bf2af5e14a1d74f591d7d22689e1389
tzisrael@deloitte.co.il <tzisrael@deloitte.co.il>
2023-06-25 10:22:55 +0000

    fix mounting

--- mount_real_estate.py
+++ mount_real_estate.py
@@ -1,32 +1,28 @@
 # Databricks notebook source
-try:
-    # Check if the mount point already exists
-    if any(mount.mountPoint == '/mnt/real_estate_short_sample_5k' for mount in dbutils.fs.mounts()):
-        # Unmount the existing mount point
-        dbutils.fs.unmount('/mnt/real_estate_short_sample_5k')
-    else:
-        # Mount the storage location
-        dbutils.fs.mount(
-            source="wasbs://testfiles@dadatabrickstorage.blob.core.windows.net",
-            mount_point="/mnt/real_estate_short_sample_5k",
-            extra_configs={
-                "fs.azure.account.key.dadatabrickstorage.blob.core.windows.net": "6pWizCt8/w9K9S+Sc9z/LNGUo/YeRy7rpUJFvBB3vv/D4W1HLyelW/+bTnuk5rGPQFzX2PFcpbUT+AStLbebqw=="
-            }
-        )
-        print("Mount successful")
-except Exception as e:
-    # Handle any errors that occur during mount
-    print("Error occurred during mount:", str(e))
-
-# COMMAND ----------
-
-try:
-    # Read the file using Python
-    file_path = "/mnt/real_estate_short_sample_5k/real_estate_short_sample_5k.csv"
-    df = spark.read.format("csv").option("header", "true").load(file_path)
-
-    # Display a limited number of rows in the DataFrame
-    display(df.limit(10))  # Displaying only the first 10 rows
-except Exception as e:
-    # Handle any errors that occur during file reading
-    print("Error occurred during file reading:", str(e))
+# MAGIC %python
+# MAGIC
+# MAGIC try:
+# MAGIC     # Check if the mount point already exists
+# MAGIC     if any(mount.mountPoint == '/mnt/real_estate_short_sample_5k' for mount in dbutils.fs.mounts()):
+# MAGIC         # Unmount the existing mount point
+# MAGIC         dbutils.fs.unmount('/mnt/real_estate_short_sample_5k')
+# MAGIC         print("Unmount successful")
+# MAGIC     else:
+# MAGIC         # Mount the storage location
+# MAGIC         dbutils.fs.mount(
+# MAGIC             source="wasbs://testfiles@dadatabrickstorage.blob.core.windows.net",
+# MAGIC             mount_point="/mnt/real_estate_short_sample_5k",
+# MAGIC             extra_configs={
+# MAGIC                 "fs.azure.account.key.dadatabrickstorage.blob.core.windows.net": "6pWizCt8/w9K9S+Sc9z/LNGUo/YeRy7rpUJFvBB3vv/D4W1HLyelW/+bTnuk5rGPQFzX2PFcpbUT+AStLbebqw=="
+# MAGIC             }
+# MAGIC         )
+# MAGIC         print("Mount successful")
+# MAGIC except Exception as e:
+# MAGIC     # Handle any errors that occur during mount
+# MAGIC     if isinstance(e, FileNotFoundError):
+# MAGIC         print("File or directory not found:", str(e))
+# MAGIC     else:
+# MAGIC         print("Error occurred during mount:", str(e))
+# MAGIC finally:
+# MAGIC     # Code to run regardless of whether an exception occurred or not
+# MAGIC     print("Mount process completed")

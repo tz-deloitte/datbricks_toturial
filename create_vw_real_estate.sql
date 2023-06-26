444dcb7f5281443a284f926f5f180113ec195b6e
tzisrael@deloitte.co.il <tzisrael@deloitte.co.il>
2023-06-26 10:14:37 +0000

    ..

---
+++ create_vw_real_estate.sql
@@ -0,0 +1,13 @@
+-- Databricks notebook source
+-- Create a temporary view
+CREATE OR REPLACE TEMPORARY VIEW vw_real_estate
+USING csv
+OPTIONS (
+  path "/mnt/real_estate_short_sample_5k/real_estate_short_sample_5k.csv",
+  header "true"
+);
+
+
+-- COMMAND ----------
+
+select * from  vw_real_estate;
